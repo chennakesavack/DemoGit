fb3bf472343984404a1631710813e243f665c765
admin <admin@ADMINRG-N5JC6AM>
2021-07-11 12:02:47 +0530

    Second class commit with new changes

--- src/pack1/Class2.java
+++ src/pack1/Class2.java
@@ -5,6 +5,7 @@ public class Class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Second Class");
+       System.out.println("Second Class one more sysout message");
 	}
 
 }
